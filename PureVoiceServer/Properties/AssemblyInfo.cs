5d35208b3a65141d3b8ed46485f7b8a21d3e2ada
c0nnex <me@c0nnex.de>
2018-11-11 15:55:34 +0100

    Bump Version

--- PureVoiceServer/Properties/AssemblyInfo.cs
+++ PureVoiceServer/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2018 c0nnex")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
